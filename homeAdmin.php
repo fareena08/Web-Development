ae5b79dc475db8751b46c58077a7a5be384a3795
Fareena Firdaus <76551048+fareena08@users.noreply.github.com>
2022-12-18 23:10:28 +0800

    Update homeAdmin.php

--- homeAdmin.php
+++ homeAdmin.php
@@ -18,7 +18,7 @@ session_start()
    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/styleHome.css">
 
 </head>
 <body>
