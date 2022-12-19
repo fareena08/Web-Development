ec4fd219a2c96dd54395278420acad744028196d
NURUL SYAFIQAH MOHD DAUD <96098405+Anonymous617@users.noreply.github.com>
2022-12-19 13:53:57 +0800

    Update service_form.php

--- service_form.php
+++ service_form.php
@@ -12,11 +12,13 @@ include_once 'service_form_crud.php';
 <!DOCTYPE html>
 <html>
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="utf-8">
+  <title>Hss Service</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="BootstrapBay">
 
-   <title>HSS: SERVICES</title>
+    <link href="assets/img/logoo.png" rel="icon" type="image/png">
 
    <!-- swiper css link  -->
    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
