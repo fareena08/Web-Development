3b3d9d1d185b2842679e8bbc25abe78f8a9676a8
NURUL SYAFIQAH MOHD DAUD <96098405+Anonymous617@users.noreply.github.com>
2022-12-19 13:53:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta charset="utf-8">
+   <title>Home Service System</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="BootstrapBay">
 
-   <title>HOME SERVICE SYSTEM</title>
+    <link href="assets/img/logoo.png" rel="icon" type="image/png">
 
    <!-- swiper css link  -->
    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
