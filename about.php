65f99cbeaf080d514d68aad72edb852f046e2a88
NURUL SYAFIQAH MOHD DAUD <96098405+Anonymous617@users.noreply.github.com>
2022-12-19 13:51:56 +0800

    Update about.php

--- about.php
+++ about.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>about</title>
+   <meta charset="utf-8">
+   <title>Hss About</title>
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="BootstrapBay">
+
+    <link href="assets/img/logoo.png" rel="icon" type="image/png">
+ 
 
    <!-- swiper css link  -->
    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
