0f746fda6d3205548e9c63ac4c9a03d6c6a6aa4b
Fareena Firdaus <76551048+fareena08@users.noreply.github.com>
2023-01-05 00:26:25 +0800

    Update database.php

--- database.php
+++ database.php
@@ -4,5 +4,7 @@ $servername = "lrgs.ftsm.ukm.my";
 $username = "a181538";
 $password = "cutewhiteturtle";
 $dbname = "a181538";
- 
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
 ?>
