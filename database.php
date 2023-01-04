52870b3578a8414c94a96c0effeb0d9e44f91157
Syafiqah Zaki <97462586+blckrse@users.noreply.github.com>
2023-01-05 00:39:17 +0800

    Update database.php

--- database.php
+++ database.php
@@ -7,4 +7,7 @@ $dbname = "a181538";
 
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
+$connect = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+$connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
 ?>
