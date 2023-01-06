1abf084c81dd92066c12ffb49f4c105534794884
Syafiqah Zaki <97462586+blckrse@users.noreply.github.com>
2023-01-07 02:04:58 +0800

    Update validateRegisterCust.php

--- validateRegisterCust.php
+++ validateRegisterCust.php
@@ -39,7 +39,7 @@
     }
 
     else if($count == 0 && $count1 == 0) {
-      $stmt = $conn->prepare("INSERT INTO tbl_customer (fld_cust_name, fld_cust_username, fld_role, fld_cust_phone, fld_cust_addr, fld_cust_email, fld_cust_pass) VALUES(:cName,:cUsername, :role, :cPhone, :cAddr, :cEmail, :cPass)");
+      $stmt = $conn->prepare("INSERT INTO tbl_customer (fld_cust_name, fld_cust_username, fld_role, fld_cust_phone, fld_cust_addr, fld_cust_email, fld_cust_pass, postdate) VALUES(:cName,:cUsername, :role, :cPhone, :cAddr, :cEmail, :cPass, :pdate)");
 
       $cName = $_POST['cName'];
       $cUsername = $_POST['cUsername'];
@@ -48,6 +48,7 @@
       $cAddr = $_POST['cAddr'];
       $cEmail = $_POST['cEmail']; 
       $cPass = $_POST['cPass'];
+      $postdate = date("Y-m-d", time());
 
       $stmt->bindParam(':cName', $cName, PDO::PARAM_STR);
       $stmt->bindParam(':cUsername', $cUsername, PDO::PARAM_STR);
@@ -56,6 +57,7 @@
       $stmt->bindParam(':cAddr', $cAddr, PDO::PARAM_STR);
       $stmt->bindParam(':cEmail', $cEmail, PDO::PARAM_STR);
       $stmt->bindParam(':cPass', $cPass, PDO::PARAM_STR);
+      $stmt->bindParam(':pdate', $postdate, PDO::PARAM_STR);
 
       $stmt->execute();
       echo "<script>alert('Welcome! You have successfully registered!');document.location='loginUser.php'</script>";
