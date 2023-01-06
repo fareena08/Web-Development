9cc2e9dfa49f29935eac36865cdbe22fe63e2220
Syafiqah Zaki <97462586+blckrse@users.noreply.github.com>
2023-01-07 00:46:42 +0800

    Update nav_bar.php

--- nav_bar.php
+++ nav_bar.php
@@ -13,7 +13,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Profile</a>
+          <a class="nav-link" href="custProfile.php">Profile</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="search.php">Search</a>
