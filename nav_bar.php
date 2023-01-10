d30af1026aeebc8d250cdb2fd31ec8526bbf510e
Syafiqah Zaki <97462586+blckrse@users.noreply.github.com>
2023-01-10 23:14:49 +0800

    Update nav_bar.php

--- nav_bar.php
+++ nav_bar.php
@@ -19,7 +19,7 @@
           <a class="nav-link" href="search.php">Search</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Book</a>
+          <a class="nav-link" href="booking_list_cust.php">Booking List</a>
         </li>
       </ul>
     </div>
