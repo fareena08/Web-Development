bc24a0b8a1a895ae325df79b553e9cf76fcb731c
nosyamimi1 <120195148+nosyamimi1@users.noreply.github.com>
2023-01-12 02:55:48 +0800

    Update validate_loginsp.php

--- validate_loginsp.php
+++ validate_loginsp.php
@@ -1,3 +1,4 @@
+
 <?php 
 
 // include_once 'database2.php';
@@ -8,45 +9,47 @@ if(empty($_SESSION)) {
   }
 
   // $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
-  //$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  //$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
   if(isset($_POST['create'])) {
 
     try {
-      $username = $_POST['username'];
-      $password = $_POST['password'];
-    
-      $stmt = mysqli_query($conn, "SELECT * from tbl_sp WHERE fld_sp_email = '$username' AND fld_sp_pass = '$password' ");
-      $row = mysqli_fetch_array($stmt);
-      
-      $stmt2 = mysqli_query($conn, "SELECT * from tbl_sp WHERE fld_sp_email = '$username' AND fld_sp_pass = '$password' ");
-      $count = mysqli_num_rows($stmt2);
-
-      if($count==0){
-         echo "<script>alert('Sorry, Your email or password not match. Please try again.');</script>";
-      }
-      elseif($count==1){
-        $status = $row['fld_sp_status'];
-        $_SESSION["status"]=$row['fld_sp_status'];
-        $_SESSION["username"]=$row['fld_sp_email'];
-        $_SESSION["password"]=$row['fld_sp_pass'];
-        $_SESSION['username'] = $row["fld_sp_name"];
-
-        if($status=="Approved"){
-          
-          echo "<script>alert('Welcome {$_SESSION['username']}! You have successfully logged in!');document.location='homeSP.php'</script>";
-        }
-        elseif ($status=="Pending") {
-          echo "<script>alert('Your account is still pending for approval!');document.location='loginSP.php'</script>";
-        }
-        elseif ($status=="Rejected") {
-          echo "<script>alert('Your account registration is rejected. Please register again with correct SSM Number!');document.location='loginSP.php'</script>";
-        }
+ 
+    $stmt = $conn->prepare("SELECT * from tbl_sp WHERE fld_sp_email = :username AND fld_sp_pass = :password ");
+   
+    $stmt->bindParam(':username', $username, PDO::PARAM_STR);
+    $stmt->bindParam(':password', $password, PDO::PARAM_STR);
        
-      }
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+         
+    $stmt->execute();
+
+    $count = $stmt->rowCount();
+
+    $readrow = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    if ($count == 0) {
+      echo "<script>alert('Sorry, Your email or password not match. Please try again.');</script>";
+    }
+
+    else if($count == 1 ) {
+      $_SESSION['fld_sp_email'] = $readrow["fld_sp_email"];
+      $_SESSION['fld_sp_name'] = $readrow["fld_sp_name"];
+      $_SESSION['fld_sp_phone'] = $readrow["fld_sp_phone"];
+      $_SESSION['fld_sp_addr'] = $readrow["fld_sp_addr"];
+      $_SESSION['fld_service_name'] = $readrow['fld_service_name'];
+      $_SESSION['fld_sp_ssm'] = $readrow["fld_sp_ssm"];
+      $_SESSION['fld_price'] = $readrow["fld_price"];
+      //$_SESSION['custusername'] = $readrow["fld_cust_username"];
+      $_SESSION['fld_image'] = $readrow["fld_image"];
       
-     
+      echo "<script>alert('Welcome {$_SESSION['fld_sp_email']}! You have successfully registered!');document.location='homeSP.php'</script>";
+      
+
+    }
+    
     
   }
   catch(PDOException $e) {
