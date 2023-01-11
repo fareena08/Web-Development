6251362caf9882c8e77c398ea5971de6641eee03
nosyamimi1 <120195148+nosyamimi1@users.noreply.github.com>
2023-01-12 02:34:55 +0800

    Update sp_profile.php

--- sp_profile.php
+++ sp_profile.php
@@ -1,131 +1,139 @@
 <?php 
-
-// include_once 'database2.php';
-include_once 'database.php';
-
-
-if(empty($_SESSION)) {
-    session_start();
-  }
-
-
-  // $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username);
+session_start();
+include "database.php";
+// include_once 'editcustProfile.php';
+// include_once 'updatecustProfile.php';
+try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+       
+  $stmt = $conn->prepare("SELECT * FROM tbl_sp WHERE fld_sp_email = '".$_SESSION['fld_sp_email']."'");
+  $stmt->execute();
 
-include_once 'sp_profile_crud.php';
-?>
+  $result = $stmt->fetchAll();
+}
+catch(PDOException $e)
+{
+        echo "Error: " . $e->getMessage();
+    }
+ 
+    $conn = null;
+?> 
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <title>Home Service System</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="author" content="BootstrapBay">
 
-   <title>HSS: MY PROFILE</title>
+    <link href="assets/img/logoo.png" rel="icon" type="image/png">
 
    <!-- swiper css link  -->
    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
 
    <!-- font awesome cdn link  -->
-   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
    <!-- custom css file link  -->
-   <link rel="stylesheet" href="css/styleProfile.css"/>
+   <link rel="stylesheet" href="css/profile.css">
 
-   
+   <!-- custom css profile link-->
+  <link rel="stylesheet" href="css/profile.css">
+
+  <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+
+<style>
+img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+}
+</style>
 
 </head>
 <body>
 
 <!-- header section starts  -->
 
-<section class="header">
+   <section class="header">
 
-      <a href="home.php" class="logo">HSS</a>
+         <a href="home.php" class="logo">HSS</a>
 
-      <nav class="navbar">
-         <a href="homeAdmin.php">home</a>
-         <a href="about.php">profile</a>
-         <a href="admin_establish.php">services</a>
-         <a href="#">Validate Account</a>
-         <a style="color:black"></a>
-         <a href="logout.php" class='fas fa-sign-out-alt'></a>
-      </nav>
+         <nav class="navbar">
+            <a href="homeCust.php">home</a>
+            <a href="custProfile.php">Profile</a>
+            <a href="#">book</a>
+            <a style="color:black"><?php echo "Hi, {$_SESSION['fld_sp_name']}!" ?></a>
+            <a href="logout.php" class='fas fa-sign-out-alt'></a>
+         </nav>
 
-  <div id="menu-btn" class="fas fa-bars"></div>
-</section>
-
-
-<!--pROFILE PAGE start-->
-<section>
-
-    <!-- ?php
-      $select = mysqli_query($conn, "SELECT * FROM 'tbl_sp' WHERE id = '$user_id'") or die('query failed');
-      if(mysqli_num_rows($select) > 0){
-         $fetch = mysqli_fetch_assoc($select);
-      }value="<?php echo $fetch['name']; ?>
-   ? >-->
-   <div class="wrapper">
+     <div id="menu-btn" class="fas fa-bars"></div>
+   </section><br>
+   <section></section>
+   <section></section>
+<section><br>
+<div class="wrapper">
     <div class="left">
-        <img src="images/jomcuci.jpg" 
-        alt="user" width="100">
-        <h4>Lawn Care</h4>
-         <p>Service Provider</p>
+        <!-- <img src="" 
+        alt="user" width="100"> -->
+
+        <!-- <img src="img/harrypic.jpeg" width="20%" height="20%" class="img-responsive img-circle margin" style="display:inline"> -->
+
+      <?php if ($_SESSION['fld_image'] == "" ) { 
+        echo '<img src="img/noprofile.jpeg">';
+      }
+      else { ?>
+      <img src="images/<?php echo $_SESSION['fld_image'] ?>" >
+      <?php } ?>
+    
+
+        <br><br>
+        <h1><a style="color:white;text-transform: uppercase;font-family: 'Josefin Sans', sans-serif;"><?php echo "{$_SESSION['fld_sp_name']}" ?></a> </h1>
+         <!-- <p>Customer</p> -->
     </div>
+
     <div class="right">
         <div class="info">
-            <h3>Information</h3>
+          <center>
+            <h1>SERVICE PROVIDER PROFILE</h1></center>
+            <br>
             <div class="info_data">
                  <div class="data">
-                  <div class="data">
-                    <h4>Service Name</h4>
-                    <p>Sofa and Mattress Cleaning</p>
+                    <h2>Service Provider Name</h2>
+                    <p><?php echo "{$_SESSION['fld_sp_name']}" ?></p>
+                    <h2>Service</h2>
+                    <p><?php echo "{$_SESSION['fld_service_name']}" ?></p>
+                    <h2>Email</h2>
+                    <p><?php echo "{$_SESSION['fld_sp_email']}" ?></p>
+                    <h2>Phone</h2>
+                    <p><?php echo "{$_SESSION['fld_sp_phone']}" ?></p>
+                    <h2>Price</h2>
+                    <p><?php echo "{$_SESSION['fld_price']}" ?></p>
+                    <h2>SSM Number</h2>
+                    <p><?php echo "{$_SESSION['fld_sp_ssm']}" ?></p>
+                    <h2>Address</h2>
+                    <p><?php echo "{$_SESSION['fld_sp_addr']}" ?></p>
                  </div>
-                    <h4>Email</h4>
-                    <p>alex@gmail.com</p>
-                 </div>
-                 <div class="data">
-                   <h4>Phone</h4>
-                    <p>0001-213-998761</p>
-              </div>
-            </div>
-        </div>
-      
-      <div class="projects">
-            <h3>Projects</h3>
-            <div class="projects_data">
-                 <div class="data">
-                    <h4>Recent</h4>
-                    <p>Lorem ipsum dolor sit amet.</p>
-                 </div>
-                 <div class="data">
-                   <h4>Most Viewed</h4>
-                    <p>dolor sit amet.</p>
-              </div>
-            </div>
-        </div>
-      
+            </div><br>
         
+            
+        <center>
+      <button class="btn btn-block btn-primary" type="submit" name="editform" onclick="window.location.href='editSpProfile.php';" ><span class="glyphicon glyphicon-plus" aria-hidden="true"></span>Edit Profile</button></center>
     </div>
 </div>
-   
-
-
 </section>
+<section></section><section></section><br><br><br><br><br><br>
 <section></section>
 <section></section>
-
 <section></section>
-
-
+<section></section>
 <section class="footer">
 
    <div class="box-container">
 
-
-
-      <div class="box">
+      <div class="box" style="text-align: center;">
          <h3>contact info</h3>
          <a href="#"> <i class="fas fa-phone"></i> 03-56212121 </a>
          <a href="#"> <i class="fas fa-phone"></i> 019-1112221 </a>
@@ -157,9 +165,3 @@ include_once 'sp_profile_crud.php';
 
 </body>
 </html>
-
-   
-
-
-
-
