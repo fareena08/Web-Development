cbec965de07d9431251c0ddea0c44c21f2117a20
nosyamimi1 <120195148+nosyamimi1@users.noreply.github.com>
2023-02-02 01:30:30 +0800

    Update booking_crud.php

--- booking_crud.php
+++ booking_crud.php
@@ -6,11 +6,12 @@ include('database.php');
 
 if (isset($_POST['create'])) {
 
-  $fld_cust_id=$_SESSION['fld_cust_id'];
+  $fld_cust_id=$_SESSION['fld_user_id'];
+  $current_date = date("d-m-Y");
  
   try {
      
-    $stmt = $connect->prepare("INSERT INTO tbl_booking(fld_booking_id, fld_user_id, fld_cust_name, fld_sp_name, fld_service_name, fld_cust_address, fld_date, fld_time, fld_price, postdate, fld_booking_status) VALUES(:bid, :fld_user_id, :fld_cust_name, :fld_sp_name, :fld_service_name, :fld_cust_address, :fld_date, :fld_time, :fld_price, :postdate, 'Pending' )");
+    $stmt = $connect->prepare("INSERT INTO tbl_booking(fld_booking_id, fld_user_id, fld_cust_name, fld_sp_name, fld_service_name, fld_cust_address, fld_date, fld_time, fld_price, postdate, fld_booking_status) VALUES(:bid, :fld_user_id, :fld_cust_name, :fld_sp_name, :fld_service_name, :fld_cust_address, :fld_date, :fld_time, :fld_price, '$current_date', 'Pending' )");
 
     $stmt->bindParam(':bid', $bid, PDO::PARAM_STR);
     $stmt->bindParam(':fld_user_id', $fld_user_id, PDO::PARAM_STR);
@@ -21,7 +22,7 @@ if (isset($_POST['create'])) {
     $stmt->bindParam(':fld_date', $fld_date, PDO::PARAM_STR);
     $stmt->bindParam(':fld_time', $fld_time, PDO::PARAM_STR);
     $stmt->bindParam(':fld_price', $fld_price, PDO::PARAM_STR);
-    $stmt->bindParam(':postdate', $postdate, PDO::PARAM_STR);
+    //$stmt->bindParam(':postdate', $postdate, PDO::PARAM_STR);
 
     //$stmt->bindParam(':fld_booking_status', $fld_booking_status, PDO::PARAM_STR);
 
@@ -34,7 +35,7 @@ if (isset($_POST['create'])) {
     $fld_date = $_POST['fld_date'];
     $fld_time = $_POST['fld_time'];
     $fld_price = $_POST['fld_price'];
-    $postdate = $_POST['postdate'];
+   // $postdate = $_POST['postdate'];
     $stmt->execute();
     //$id = $connect -> lastInsertId(); 
 
